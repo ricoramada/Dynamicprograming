055b489baca04dc82d369da2ccf8b7b9e1e7de8b
ASUS <ASUS@DESKTOP-MSAH19U>
2018-09-10 10:01:21 +0700

    Tambah Input

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -1,5 +1,5 @@
 package DynamicProgramming;
-
+import java.util.*;
 public class HitungFibonacci {
     private static void tampilJudul(String identitas)
     {
@@ -7,5 +7,13 @@ public class HitungFibonacci {
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21,... dst.\n");
+    }
+    private static int tampilInput()
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = scan.nextInt();
+        return n;
     }
 }
